5192b241ddab2798ed241ebddb6dd0261cffd922
Brian Purgert <brianp0488@gmail.com>
2022-01-25 21:31:11 -0500

    Basic functionality

--- src/PlanningPoker.Blazor/Server/Hubs/PokerHub.cs
+++ src/PlanningPoker.Blazor/Server/Hubs/PokerHub.cs
@@ -5,8 +5,27 @@ namespace PlanningPoker.Blazor.Server.Hubs;
 
 public class PokerHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string user, string message, string groupName)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+
     }
+
+    public async Task AddToGroup(string groupName)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+    }
+
+  public async Task ShowResults(string groupName)
+  {
+    await Clients.Group(groupName).SendAsync("ShowResults");
+  }
+
+  public async Task Reset(string groupName)
+  {
+    await Clients.Group(groupName).SendAsync("Reset");
+  }
+
 }
