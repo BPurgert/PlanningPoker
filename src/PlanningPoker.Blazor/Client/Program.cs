4dd37f8b9c464a18538d1502f452f46e00055bdb
Brian Purgert <brianp0488@gmail.com>
2022-01-27 22:03:21 -0500

    Syncfusion and UI design

--- src/PlanningPoker.Blazor/Client/Program.cs
+++ src/PlanningPoker.Blazor/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlanningPoker.Blazor.Client;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTczMTkyQDMxMzkyZTM0MmUzMFcxT1VHby9lWU11RXdZYjVjWE9KU09ab21YLytRZkZzanhKcER6OVdaRFk9");
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 await builder.Build().RunAsync();
